a05efc30842791200f839ab673af48bea23d8d10
Shaan Singh <44022060+HelixAssassin@users.noreply.github.com>
2019-10-18 15:30:08 +0100

    Main Menu and Level Changes
    I've made a temporary main menu and have also made some few changes to the number of levels in the game

---
+++ Assets/Scripts/menuScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour { 
+
+    public void loadMain ()
+{
+        SceneManager.LoadScene(1);
+}
+
+    public void exitGame ()
+    {
+        Application.Quit ();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
