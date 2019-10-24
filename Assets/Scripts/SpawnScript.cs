7eb47e3d1c8a9418530f44e8c184117bcdbc87eb
Shaan Singh <44022060+HelixAssassin@users.noreply.github.com>
2019-10-24 11:41:53 +0100

    Scripting Changes
    Added a few scripts for the UI and the Spawning.

---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SpawnScript : MonoBehaviour
+{
+
+
+
+    [SerializeField] GameObject thingToSpawn;
+    [SerializeField] float delayBetweenSpawns = 2.0f;
+    [SerializeField] float timeOfNextSpawn = 1f;
+
+    int amountToSpawn = 10;
+    static int AmountSpawned = 1;
+
+    void Start()
+    {
+
+    }
+
+
+
+
+    void Update()
+
+    {
+
+        if (Time.time >= timeOfNextSpawn && AmountSpawned < amountToSpawn)
+
+        {
+
+            Instantiate(thingToSpawn, transform.position, Quaternion.identity);
+
+            timeOfNextSpawn = Time.time + delayBetweenSpawns;
+
+
+
+        }
+
+    }
+
+}
